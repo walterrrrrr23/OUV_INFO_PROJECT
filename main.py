e263c74c84c622e30166819bfe079a1234e263be
Askeladd1 <Arkannum@icloud.com>
2026-01-28 15:36:34 +0100

    Askaladd test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
 from game import Game
 
 def main():
+    print("test")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("test")
