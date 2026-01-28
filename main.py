8900844a02ec6c9a34ca2f20394dee3532a5b298
Askeladd1 <Arkannum@icloud.com>
2026-01-28 15:51:31 +0100

    supression test

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import pygame
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
 from game import Game
 
-#bonjour
-
 def main():
-    print("test")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("test")
