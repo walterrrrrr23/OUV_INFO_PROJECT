1c09c54a6d422c007679a0de674226b3811191ca
mathislemineur <mathis.lemineur@gmail.com>
2026-02-20 16:57:04 +0100

    menu gameover

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import pygame
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, PAUSE
 from game import Game
-from menu_pause import pause, crea_boutons, ca_clique
+from menu_pause import pause, crea_boutons_pause, ca_clique, gameover, crea_boutons_gameover
 
 def main():
     pygame.init()
@@ -13,8 +13,10 @@ def main():
     game = Game()
 
     ppause = False
+    estmort = False
     running = True
-    les_boutons = crea_boutons()
+    les_boutons_pause = crea_boutons_pause()
+    les_boutons_gameover = crea_boutons_gameover()
     
     while running:
         dt = clock.tick(FPS) / 1000 #conversion en s
@@ -37,8 +39,10 @@ def main():
 
             # gestion souris
 
-            if ppause:
-                action = ca_clique(event, les_boutons)
+            if ppause or estmort:
+
+                action = ca_clique(event, les_boutons_pause)
+
                 if action == "resume":
                     ppause = False
 
@@ -52,7 +56,7 @@ def main():
         # si on est en pause, on affiche le menu
         if ppause :
             game.draw(screen)
-            pause(screen, les_boutons)
+            pause(screen, les_boutons_pause)
         
         # si on est pas en pause, on update
         else:
@@ -62,7 +66,9 @@ def main():
         # gestion du gameover
 
             if game.player.health <= 0 :
-                running = False #METTRE LOGIQUE GAME_OVER
+                estmort = not estmort
+                gameover(screen, les_boutons_gameover)
+                #running = False #METTRE LOGIQUE GAME_OVER
 
         pygame.display.flip() #screen update
 
