d2daf6121d31843e92a9567fbc87f2e6de995e58
mathislemineur <mathis.lemineur@gmail.com>
2026-03-16 15:04:34 +0100

    piti bug

--- game.py
+++ game.py
@@ -56,8 +56,8 @@ class Game:
 
     def spawn(self):
         #for i in range((self.stage)):
-        pos_x = self.camera.offset.x + SCREEN_WIDTH/2 + randint(-1,1)*randint(SCREEN_WIDTH/2, SCREEN_WIDTH)
-        pos_y = self.camera.offset.y + SCREEN_HEIGHT/2 + randint(-1,1)*randint(SCREEN_HEIGHT/2, SCREEN_HEIGHT)
+        pos_x = self.camera.offset.x + SCREEN_WIDTH//2 + randint(-1,1)*randint(SCREEN_WIDTH//2, SCREEN_WIDTH)
+        pos_y = self.camera.offset.y + SCREEN_HEIGHT//2 + randint(-1,1)*randint(SCREEN_HEIGHT//2, SCREEN_HEIGHT)
         self.enemy = Enemy((pos_x, pos_y), self.camera, self.player, "Joker", self.sprite_coins, self.sprite_bullets, self.damage_indicator)
         print(pos_x, pos_x)
         self.sprite_mob.add(self.enemy)
